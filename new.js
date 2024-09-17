ddc0adc607296ceb7a0a00e54d782d24cedd9d68
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2024-09-17 09:05:46 +0530

    practise some operations and more

--- new.js
+++ new.js
@@ -1,1 +1,17 @@
 console.log("sachintha lakshan")
+
+let num = 23
+num = num + 10
+console.log(num)
+
+let age = 12
+if(age>18){
+    console.log("you can watch it")
+}
+else{
+    console.log("you cant watch it")
+}
+
+for(let i =0;i<=100;i++){
+    console.log("sachintha lakshan",i)
+}
