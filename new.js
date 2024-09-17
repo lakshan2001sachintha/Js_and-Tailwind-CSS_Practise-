667b2709b572ed1cf3549ec909fb736a1a43add0
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2024-09-17 09:18:45 +0530

    practise switch cases and more

--- new.js
+++ new.js
@@ -1,8 +1,12 @@
 console.log("sachintha lakshan")
 
+//////////////////////////////
+
 let num = 23
 num = num + 10
 console.log(num)
+
+//////////////////////////////
 
 let age = 12
 if(age>18){
@@ -15,3 +19,31 @@ else{
 for(let i =0;i<=100;i++){
     console.log("sachintha lakshan",i)
 }
+
+////////////////////////////////
+
+let day = 3;
+switch(day){
+   case 0:
+    day = "Sunday";
+    break;
+   case 1:
+    day = "Monday";
+    break;
+   case 2:
+    day = "Tuesday";
+    break;  
+    case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday";
+}
+console.log("today is : "+day)
+
